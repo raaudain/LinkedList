11031dd10af3d0c8680ef8081a26bfb6b608af7d
Ramon Audain <raudain123@gmail.com>
2020-07-11 02:22:43 +0000

    Create addToTail method

--- main.js
+++ main.js
@@ -10,16 +10,16 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 
-// Each object below is a linked list
-const LL = new LinkedList();
-const userList = new LinkedList();
-const toDoList = new LinkedList();
+// // Each object below is a linked list
+// const LL = new LinkedList();
+// const userList = new LinkedList();
+// const toDoList = new LinkedList();
 
-// Each object below is a new node
-const node1 = new Node(100, "node2", null);
+// // Each object below is a new node
+// const node1 = new Node(100, "node2", null);
 
-console.log(LL)
-console.log(node1)
+// console.log(LL)
+// console.log(node1)
 
 LinkedList.prototype.addToHead = function(value) {
   const newNode = new Node(value, this.head, null);
@@ -36,3 +36,25 @@ LinkedList.prototype.addToHead = function(value) {
   // Make newNode head
   this.head = newNode;
 }
+
+
+LinkedList.prototype.addToTail = function(value){
+  const newNode = new Node(value, null, this.tail)
+
+  if (this.tail){
+    this.tail.next = newNode;
+  }
+  else {
+    this.head = newNode;
+  }
+
+  this.tail = newNode;
+
+}
+
+const ll = new LinkedList();
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+
+console.log(ll)
